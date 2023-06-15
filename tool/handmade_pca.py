e98c35a01bbe8f6af4e23150b197bc9d10730dd6
YOUNGSEOK SEO <sysconan@gmail.com>
2023-06-15 18:53:34 +0900

    Handmad PCA Class & Example Code

---
+++ tool/handmade_pca.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import numpy as np
+import matplotlib.pyplot as plt
+from imblearn.over_sampling import SMOTE
+
+class handmade_pca:
+    def __init__(self, df):
+        # Desingnate x & y Variable
+        self.df = df
+        print('변수:')
+        for idx, col in enumerate(df.columns.tolist()):
+            print(idx+1, col)
+        print("\n'.하나 이상의 독립변수와 종속변수 하나를 지정해주세요.")
+        print("위의 보기를 확인하고 독립변수들의 번호를 x, 종속변수의 번호를 y에 넣어주세요.")
+        print("지정 후, .set_data()를 통해 분석을 위한 데이터를 생성해주세요.")
+        print("ex1) x: 1 2 3 4 5 8 9 10 / y = 46")
+        print("ex2) x: 1:5 8:10 / y = 46")
+        print('ex3) x: 1:5 8 9 10 / y = 46')
+        
+        try:
+            x = input('x: ')
+            x = x.split(' ')
+
+            origin = x[0]
+            for i in range(len(x)):
+                if origin != x[i]:
+                    i += 2
+                if i != len(x)-1:
+                    origin = x[i+1]
+                if ':' in str(x[i]):
+                    x[i] = x[i].split(':')
+                    t = [j for j in range(int(x[i][0])-1,int(x[i][1]))]
+                    x.pop(i)
+                    for k in sorted(t,reverse=True):
+                        x.insert(i, k)
+
+                else:
+                    x[i]=int(x[i])
+                    x[i] -= 1
+
+            y = int(input('y: '))
+            y -= 1
+            y = [y]
+
+            self.x = x
+            self.y = y
+            
+        except:
+            print('번호를 다시 한 번 확인해주세요.')
+        
+        
+    def set_data(self):
+        # Generate DataFrame with Choosen Variable
+        try:
+            self.x_data = self.df.iloc[:,self.x]
+            self.y_data = self.df.iloc[:,self.y]
+
+            self.df = pd.concat([self.x_data,self.y_data],axis=1)
+
+            y_count = self.y_data.iloc[:,0].value_counts()
+            for val in range(len(y_count)):
+                if round(y_count[val]/len(self.y_data),2) != round(1/len(y_count),2):
+                    print("데이터 불균형이 존재합니다. '.smote()'를 통해 데이터 불균형을 해소하세요")
+                    print('SMOTE는 최근접 이웃(k-NN) 알고리즘을 기반으로 하여 데이터를 새로 생성해 종속변수 데이터가 같은 비율을 갖게 하는 알고리즘 입니다.')
+                    break
+                else:
+                    print('.pca()를 통해 주성분분석을 진행하세요.')
+
+            return self.df
+    
+        except:
+            print('앞서 입력하신 번호가 중복되거나 범위 안에 있는지 다시 한 번 확인해주세요.')
+            
+    def smote(self):
+        # Relaxing Data Unbalance with SMOTE
+        x_arr = self.df.iloc[:,:-1].to_numpy()
+        y_arr = self.df.iloc[:,-1].to_numpy()
+        
+        smote = SMOTE(random_state=42)
+        new_x_arr,new_y_arr = smote.fit_resample(x_arr,y_arr)
+        print('기존 데이터 형태: ', x_arr.shape, y_arr.shape)
+        print('SMOTE 적용 후 데이터 형태: ', new_x_arr.shape, new_y_arr.shape)
+        print('SMOTE 적용 후 종속변수 데이터 분포: \n', pd.Series(new_y_arr).value_counts())
+        
+        x_df = pd.DataFrame(new_x_arr, columns=self.df.columns[:-1])
+        y_df = pd.DataFrame(new_y_arr, columns=[self.df.columns[-1]])
+        self.df = pd.concat([x_df,y_df], axis=1)
+        
+        print("\n데이터 불균형이 해소 되었습니다. .pca()를 통해 주성분분석을 진행하세요.")
+        
+        return self.df
+        
+    def pca(self):
+        # Choose Number of Components & Do PCA
+        try:
+            col = self.x_data.columns
+            x = StandardScaler().fit_transform(self.x_data)
+
+            features =x.T
+            cov_mat = np.cov(features)
+
+            values, vectors = np.linalg.eig(cov_mat)
+
+            explained_variances = []
+            for i in range(len(values)):
+                explained_variances.append(values[i] / np.sum(values))
+            explained_variances = sorted(explained_variances, reverse=True) 
+
+            plt.plot(explained_variances)
+            plt.xlabel('Number of Components')
+            plt.ylabel('Explained Variance')
+            plt.title('Scree Plot')
+            plt.xticks(np.arange(0,len(explained_variances),2))
+            plt.show()
+
+            df_for_explain = pd.DataFrame({'고유값' : np.array(values),
+                               '기여율(설명력)' : np.array(explained_variances)})
+            df_for_explain['누적 기여율'] = df_for_explain['기여율(설명력)'].cumsum()
+
+            idx = df_for_explain.index
+            df_for_explain = df_for_explain.T
+            df_for_explain.columns = [f'PC{i+1}' for i in idx]
+
+            for col in range(len(df_for_explain.columns)):
+                if df_for_explain.iloc[2,col] > 0.8:
+                    num_PC = col+1
+                    break
+
+            print(df_for_explain)
+            print("\n위의 'Scree Plot'과 표를 참고하여 주성분 개수를 입력해주세요.")
+            print("일반적으로 Scree Plot의 기울기가 완만해지는 시점이나 누적 기여율이 80%~90%인 시점에서 주성분 개수를 결정합니다.")
+            print(f'추천 주성분 개수: {num_PC}')
+            compo = int(input("주성분 개수: "))
+
+            do_pca = PCA(n_components=compo)
+            printcipalComponents = do_pca.fit_transform(x)
+            pca_df = pd.DataFrame(data=printcipalComponents,
+                                  columns = [f'PC{num+1}' for num in range(len(printcipalComponents[0]))])
+            self.pca_df = pd.concat([pca_df,self.y_data],axis=1)
+
+            return self.pca_df
+        
+        except:
+            print('입력하신 주성분 개수를 다시 한 번 확인해주세요')
+
